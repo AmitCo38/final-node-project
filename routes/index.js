cbfabadef9ca98a1aadb0976cc4010ce9ba74a87
Amit Cohen <amit-mac@Amits-MacBook-Pro.local>
2026-06-16 17:44:37 +0300

    api trivia addition, and response editing

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import itemsRoutes from "./items.routes.js";
 import quotesRoutes from "./quotes.routes.js";
+import { getTriviaQuestions } from "../controllers/quotes.controller.js";
 
 const router = express.Router();
 
 router.use("/movies", itemsRoutes);
 router.use("/quotes", quotesRoutes);
+
+router.get("/trivia/:type/:difficulty/:number", getTriviaQuestions);
 
 export default router;
